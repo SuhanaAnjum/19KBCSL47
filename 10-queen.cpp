ad74261904296abb3af9775afd991a77d05a7f5d
Suhana Anjum <97942862+SuhanaAnjum@users.noreply.github.com>
2022-08-23 22:36:46 +0530

    typo

--- 10-queen.cpp
+++ 10-queen.cpp
@@ -6,7 +6,7 @@ int a[30], count = 0;
 
 int place(int pos) {
     for (int i = 1; i < pos; i++)
-        if(a[i] == a[pos] || (abs(a[i] - a[pos]) == abs(i - pos)))
+        if (a[i] == a[pos] || abs(a[i] - a[pos]) == abs(i - pos))
             return 0;
     return 1;
 }
@@ -15,7 +15,7 @@ void print_sol(int n) {
     printf("Solution #%d\n", ++count);
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            if(a[i] == j) printf("Q\t");
+            if (a[i] == j) printf("Q\t");
             else printf("*\t");
         }
         printf("\n");
@@ -25,12 +25,12 @@ void print_sol(int n) {
 void queen(int n) {
     int k = 1;
     a[k] = 0;
-    while(k != 0) {
+    while (k != 0) {
         a[k]++;
-        while(a[k] <= n && !place(k))
+        while (a[k] <= n && !place(k))
             a[k]++;
-        if(a[k] <= n)
-            if(k == n) print_sol(n);
+        if (a[k] <= n)
+            if (k == n) print_sol(n);
             else a[++k] = 0;
         else k--;
     }
